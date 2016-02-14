5eec2adba2b503b7efe0c67c4a471e341cef4602
fabianrios <hola@fabianrios.co>
2016-02-14 00:06:14 -0500

    changes css

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -8,7 +8,7 @@ module.exports = function (app) {
 
 router.get('/', function (req, res, next) {
   db.Article.findAll().then(function (articles) {
-    console.log(articles);
+    // console.log(articles);
     res.render('index', {
       title: 'wtf?',
       articles: articles
@@ -19,7 +19,7 @@ router.get('/', function (req, res, next) {
 router.get('/article/:id', function (req, res, next) {
   var id = req.params.id
   db.Article.findById(id).then(function (article) {
-    console.log(article);
+    // console.log(article);
     res.render('show', {
       title: "Pantalla individual",
       article: article
@@ -27,3 +27,43 @@ router.get('/article/:id', function (req, res, next) {
   });
 });
 
+
+
+router.get('/article/:id/edit', function (req, res, next) {
+  var id = req.params.id
+  db.Article.findById(id).then(function (article) {
+    res.render('edit', {
+      title: "Edición",
+      article: article
+    });
+  });
+});
+
+
+router.post('/articles', function (req, res, next) {
+  var body = req.body
+  console.log(body);
+  db.Article.create({ title: body.title, text: body.text, url: 'fort-knox' }).then(function () {
+   res.redirect('/');
+  });
+});
+
+router.post('/article/:id/editar', function (req, res, next) {
+  var id = req.params.id
+  var body = req.body
+  db.Article.findById(id).then(function (article) {
+    article.update({ title: body.title, text: body.text, url: 'edited' }).then(function () {
+     res.redirect('/');
+    });
+  });
+});
+
+router.get('/article/:id/destroy', function (req, res, next) {
+  db.Article.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function() {
+      res.redirect('/');
+    });
+});
