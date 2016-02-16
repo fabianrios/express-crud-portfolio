4baa45a5152404bbbbdd0139dee71771ab48643b
fabianrios <hola@fabianrios.co>
2016-02-15 21:37:43 -0500

    back

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var app = express();
 require('./config/express')(app, config); 
 
 db.sequelize
-  .sync({force: true})
+  .sync()
   .then(function () {
     app.listen(config.port, function () {
       console.log('Express server listening on port ' + config.port);
